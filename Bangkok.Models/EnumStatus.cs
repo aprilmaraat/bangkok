01930f4cd5273aa82a2aa2ccfbe1425333511ca7
AJ_Maraat <amaraat@cyrious.com>
2020-02-05 16:15:30 +0800

    Model setup.

---
+++ Bangkok.Models/EnumStatus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bangkok.Models
+{
+    public enum Status : byte
+    {
+        /// <summary>
+        /// CSV: Approved; XML: Approved;
+        /// </summary>
+        A = 1,
+        /// <summary>
+        /// CSV: Failed; XML: Rejected;
+        /// </summary>
+        R = 2,
+        /// <summary>
+        /// CSV: Finished; XML: Done;
+        /// </summary>
+        D = 3,
+    }
+    public class EnumStatus
+    {
+        public Status ID { get; set; }
+        public char StatusInitial { get; set; }
+    }
+}
---
+++ Bangkok.Models/TransactionData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bangkok.Models
+{
+    public class TransactionData
+    {
+        public string ID { get; set; }
+        public decimal Amount { get; set; }
+        public string CurrencyCode { get; set; }
+        public DateTime TransactionDT { get; set; }
+        public Status Status { get; set; }
+        /// <summary>
+        /// Navigation property to EnumStatus
+        /// </summary>
+        public virtual EnumStatus EnumStatus { get; set; }
+    }
+}
