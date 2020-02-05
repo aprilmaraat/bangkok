6321f8b607d742b063ea05649b648114bb5f0c72
AJ_Maraat <amaraat@cyrious.com>
2020-02-05 17:09:33 +0800

    EF Migration setup; Added initila migrations.

---
+++ Bangkok.EF/BangkokContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bangkok.Models;
+
+namespace Bangkok.EF
+{
+    public class BangkokContext : DbContext
+    {
+        public BangkokContext(DbContextOptions<BangkokContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<EnumStatus> EnumStatus { get; set; }
+        public virtual DbSet<TransactionData> TransactionData { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.EnableSensitiveDataLogging(false);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EnumStatus>(entity =>             
+            {
+                entity.ToTable("enum.Status");
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.ID)
+                    .HasColumnType("tinyint")
+                    .ValueGeneratedNever();
+                entity.Property(e => e.StatusInitial)
+                    .HasColumnType("nvarchar(1)");
+            });
+            modelBuilder.Entity<TransactionData>(entity => 
+            {
+                entity.ToTable("Transaction.Data");
+                entity.HasKey(e => e.ID);
+                entity.Property(e => e.ID);
+                entity.Property(e => e.Amount)
+                    .HasColumnType("decimal");
+                entity.Property(e => e.CurrencyCode)
+                    .HasColumnType("nvarchar(10)");
+                entity.Property(e => e.TransactionDT);
+                entity.Property(e => e.Status)
+                    .HasColumnType("tinyint");
+
+                entity.HasOne(e => e.EnumStatus)
+                    .WithOne()
+                    .HasForeignKey<TransactionData>(e => e.Status)
+                    .OnDelete(DeleteBehavior.Restrict)
+                    .HasConstraintName("FK_Transaction.Data_enum.Status");
+            });
+        }
+    }
+}
---
+++ Bangkok.EF/Migrations/20200205090446_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bangkok.EF.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "enum.Status",
+                columns: table => new
+                {
+                    ID = table.Column<byte>(type: "tinyint", nullable: false),
+                    StatusInitial = table.Column<string>(type: "nvarchar(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_enum.Status", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transaction.Data",
+                columns: table => new
+                {
+                    ID = table.Column<string>(nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal", nullable: false),
+                    CurrencyCode = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    TransactionDT = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transaction.Data", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Transaction.Data_enum.Status",
+                        column: x => x.Status,
+                        principalTable: "enum.Status",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transaction.Data_Status",
+                table: "Transaction.Data",
+                column: "Status",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transaction.Data");
+
+            migrationBuilder.DropTable(
+                name: "enum.Status");
+        }
+    }
+}
